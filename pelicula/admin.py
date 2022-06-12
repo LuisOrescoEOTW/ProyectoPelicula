c4e697923e36156c5b9019bbeec2c22df6a558f6
Lorescovich <lhorescovich@gmail.com>
2022-06-12 02:39:32 -0300

    Actualizacion de todo

--- pelicula/admin.py
+++ pelicula/admin.py
@@ -1,12 +1,27 @@
+from re import search
 from django.contrib import admin
-from .models import Director, Actor, Pelicula, Reseña, Administrador
+from .models import Director, Actor, Pelicula, Reseña
+from django.utils.html import format_html
 
 
 # Register your models here.
+#admin.site.register(Actor)
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    
+    list_display = ('id', 'nombre', 'nacionalidad', 'foto', 'fotos', 'año_nacimiento', 'resumen')
+    search_fields = ('nombre', 'nacionalidad') #Buscar
+    #list_editable = ('nombre', 'nacionalidad', 'año_nacimiento', 'resumen') #Editar campos
+    list_filter = ('nacionalidad',) #Añade filtros
+    #list_per_page = 3 #Paginación, solo 3 por página
+    
+    def fotos(self, obj):
+        return format_html('<img scr=obj.foto.url width="130" height="100" />')
+
+
+
 admin.site.register(Director)
-admin.site.register(Actor)
 admin.site.register(Pelicula)
 admin.site.register(Reseña)
-admin.site.register(Administrador)
 
 
