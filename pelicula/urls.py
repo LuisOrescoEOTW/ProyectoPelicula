8aec32f5109360c6f26f0949c02baea6c59c606c
Lorescovich <lhorescovich@gmail.com>
2022-07-11 17:46:47 -0300

    Actualizacion PP con buscar

--- pelicula/urls.py
+++ pelicula/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pelis)    
+    path('', views.pelis),
+    path('^actores$', views.actore),  
+    #url(r'^controlpanel$', MenuAdministrador, name='controlpanel'),
 ]
--- pelicula/views.py
+++ pelicula/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Director, Actor, Pelicula, Reseña
+
+
 
 # Create your views here.
 
+
+
 def pelis(request):
+    busqueda = request.GET.get("buscar")
     peliculasListados = Pelicula.objects.all()[:12]
+    if busqueda:
+        peliculasListados = Pelicula.objects.filter(
+            Q(nombre__icontains = busqueda)|
+            Q(puntaje__icontains = busqueda)
+        ).distinct()
     return render(request, "base.html", {"peliculas": peliculasListados})
+
+def actore(request):
+    busqueda = request.GET.get("buscar")
+    actoresListados = Actor.objects.all()
+    if busqueda:
+        actoresListados = Actor.objects.filter(
+            Q(nombre__icontains = busqueda)|
+            Q(nacionalidad__icontains = busqueda)
+        ).distinct()
+    return render(request, "actores.html", {"actores": actoresListados})
+
+
