89a0ff3a7bbcd5434ccba4778675af7f2e3f4910
Lorescovich <lhorescovich@gmail.com>
2022-07-13 12:52:37 -0300

    Enlaces

--- pelicula/urls.py
+++ pelicula/urls.py
@@ -2,15 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('resen', views.resen, name='resen'),  
     path('', views.pelis, name='pelis'),
-      
     path('actores', views.actore, name='actores'),     
-    path('directores', views.directore, name='directores'),
-=======
-    path('', views.pelis),
-    path('^actores$', views.actore),  
-    #url(r'^controlpanel$', MenuAdministrador, name='controlpanel'),
->>>>>>> 8aec32f5109360c6f26f0949c02baea6c59c606c
+    path('directores', views.directore, name='directores'),      
 ]
